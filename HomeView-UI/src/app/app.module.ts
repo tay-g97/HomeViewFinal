7777b8071c2beb33e5521c87b3b0c70dc7e30820
tay-g97 <taylorgeorgeuk@yahoo.co.uk>
2022-06-06 14:53:23 +0100

    angular user models created + account

--- HomeView-UI/src/app/app.module.ts
+++ HomeView-UI/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ HomeView-UI/src/app/models/account/user-create.model.ts
@@ -0,0 +1,19 @@
+export class UserCreate {
+  constructor(
+    public firstname: string,
+    public lastname: string,
+    public dateofbirth: string,
+    public addressline1: string,
+    public addressline2: string,
+    public addressline3: string,
+    public town: string,
+    public city: string,
+    public postcode: string,
+    public accounttype: string,
+    public email: string,
+    public phone: string,
+    public marketingEmail: string,
+    public marketingPhone: boolean,
+    public profilepictureId?: number
+  ) {}
+}
